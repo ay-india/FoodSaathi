7fecb15d4098880dc84634a1545decdcccf865b9
ay-india <ashishyd240@gmail.com>
2023-02-27 19:07:04 +0530

    foodSaathi

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -3,11 +3,13 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_delivery_app/controller/cart_controller.dart';
 import 'package:food_delivery_app/screens/cart_page.dart';
+import 'package:food_delivery_app/screens/homepage.dart';
 import 'package:food_delivery_app/screens/user_account.dart';
 import 'package:food_delivery_app/utils/app_constants.dart';
 import 'package:get/get.dart';
 
 import '../controller/recommended_product_controller.dart';
+import '../routes/routes_helper.dart';
 
 class ProductPage extends StatelessWidget {
   int pageId;
@@ -21,188 +23,200 @@ class ProductPage extends StatelessWidget {
         .initProduct(product, Get.find<CartController>());
     Get.put(() => CartController(cartRepo: Get.find()));
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            padding: EdgeInsets.only(
-              top: 54.7.h,
-              left: 8.w,
-              right: 12.w,
-            ),
-            alignment: Alignment.topLeft,
-            height: 39.h,
-            color: Color.fromARGB(255, 221, 164, 79),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: IgnorePointer(
-                    child: Icon(
-                      Icons.arrow_back,
-                      size: 31.r,
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const CartPage(),
-                      ),
-                    );
-                    print(MediaQuery.of(context).size.height);
-                  },
-                  child: IgnorePointer(
-                    child: Icon(
-                      Icons.shopping_cart,
-                      size: 33.r,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          Padding(
-            padding: EdgeInsets.only(
-              top: 250.h,
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 180.h),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(
-                    31.r,
-                  ),
-                  topRight: Radius.circular(
-                    31.r,
-                  ),
-                ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: 9.7.h,
+                left: 8.w,
+                right: 12.w,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              alignment: Alignment.topLeft,
+              height: 40.h,
+              // color: Color.fromARGB(255, 221, 164, 79),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // product Description
-                  Text(
-                    product.description!,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 4,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 19.sp),
-                  ),
-                  SizedBox(
-                    height: 30.h,
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.only(left: 20.w, right: 20.w, bottom: 15.h),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          product.name!,
-                          style: TextStyle(
-                            fontSize: 30.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  Material(
+                    child: InkWell(
+                      onTap: () {
+                        Get.to(() => HomePage());
+                      },
+                      child: Container(
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 31.r,
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '300g/530kcal',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.sp,
-                              ),
-                            ),
-                            Text(
-                              '1 portion',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.sp,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                  Divider(
-                    indent: 10.w,
-                    endIndent: 10.w,
-                    thickness: 3.h,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 20.w,
-                      right: 20.w,
-                      top: 13.h,
-                      bottom: 13.h,
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const CartPage(),
+                        ),
+                      );
+                    },
+                    child: Material(
+                      child: Icon(
+                        Icons.shopping_cart,
+                        size: 33.r,
+                      ),
                     ),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            '1.3 km from you',
-                            style: TextStyle(fontSize: 17.sp),
-                          ),
-                          RatingBar.builder(
-                            itemSize: 24.r,
-                            initialRating: 3,
-                            minRating: 1,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            itemCount: 5,
-                            itemPadding:
-                                EdgeInsets.symmetric(horizontal: 4.0.w),
-                            itemBuilder: (context, _) => const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                            ),
-                            onRatingUpdate: (rating) {
-                              print(rating);
-                            },
-                          ),
-                        ]),
-                  ),
-                  Divider(
-                    indent: 10.w,
-                    endIndent: 10.w,
-                    thickness: 3,
                   ),
                 ],
               ),
             ),
-          ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 65.h),
+                // color: Colors.red,
+                child: Stack(
+                  // fit: StackFit.loose,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 170.h,
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.only(top: 180.h),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 246, 237, 221),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(
+                              31.r,
+                            ),
+                            topRight: Radius.circular(
+                              31.r,
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // product Description
+                            Text(
+                              product.description!,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 19.sp),
+                            ),
+                            SizedBox(
+                              height: 10.h,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  left: 20.w, right: 20.w, bottom: 15.h),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    product.name!,
+                                    style: TextStyle(
+                                      fontSize: 30.sp,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '300g/530kcal',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16.sp,
+                                        ),
+                                      ),
+                                      Text(
+                                        '1 portion',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16.sp,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              indent: 10.w,
+                              endIndent: 10.w,
+                              thickness: 3.h,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: 20.w,
+                                right: 20.w,
+                                top: 13.h,
+                                bottom: 13.h,
+                              ),
+                              child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '1.3 km from you',
+                                      style: TextStyle(fontSize: 17.sp),
+                                    ),
+                                    RatingBar.builder(
+                                      itemSize: 24.r,
+                                      initialRating: 3,
+                                      minRating: 1,
+                                      direction: Axis.horizontal,
+                                      allowHalfRating: true,
+                                      itemCount: 5,
+                                      itemPadding: EdgeInsets.symmetric(
+                                          horizontal: 4.0.w),
+                                      itemBuilder: (context, _) => const Icon(
+                                        Icons.star,
+                                        color: Colors.amber,
+                                      ),
+                                      onRatingUpdate: (rating) {
+                                        print(rating);
+                                      },
+                                    ),
+                                  ]),
+                            ),
+                            Divider(
+                              indent: 10.w,
+                              endIndent: 10.w,
+                              thickness: 3,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
 
-          ///White Conatiner part
+                    ///White Conatiner part
+                    ///
+                    ///product imagae part
+                    ///
 
-          Positioned(
-            top: 119.h,
-            left: 59.w,
-            child: CircleAvatar(
-              radius: 140.h,
-              // backgroundColor: Colors.blue,
-              backgroundImage: NetworkImage(
-                  AppConstants.baseUrl + '/uploads/' + product.img!),
+                    Positioned(
+                      // top: 7.h,
+                      left: 10.w,
+                      child: CircleAvatar(
+                        radius: 170.h,
+                        // backgroundColor: Colors.blue,
+                        backgroundImage: NetworkImage(
+                            AppConstants.baseUrl + '/uploads/' + product.img!),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-          // Column(
-          //   children: [
-          //     Container(
-          //       height: 300,
-          //       color: Colors.orangeAccent,
-          //     )
-          //   ],
-          // )
-        ],
+          ],
+        ),
       ),
+
+      ///bottom navigation part // add to cart and item increase
       bottomNavigationBar: GetBuilder<RecommendedProductController>(
         builder: (recommendedProduct) {
           return Container(
@@ -214,7 +228,7 @@ class ProductPage extends StatelessWidget {
               bottom: 14.h,
             ),
             decoration: BoxDecoration(
-              color: Color.fromARGB(255, 249, 241, 230),
+              color: Color.fromARGB(255, 249, 239, 226),
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(18.r),
                 topRight: Radius.circular(18.r),
@@ -295,15 +309,6 @@ class ProductPage extends StatelessWidget {
                           fontSize: 17.sp,
                         ),
                       ),
-                      // SizedBox(
-                      //   width: 13.h,
-                      // ),
-                      // CircleAvatar(
-                      //   child: Icon(
-                      //     Icons.add,
-                      //     color: Colors.grey,
-                      //   ),
-                      // ),
                     ),
                   ],
                 )
@@ -315,25 +320,3 @@ class ProductPage extends StatelessWidget {
     );
   }
 }
-
-// Container(
-//           color: Colors.red,
-//           child: Column(
-//             children: [
-//               Container(
-//                 // margin: EdgeInsets.only(top: 15),
-//                 padding: EdgeInsets.only(left: 10, right: 10),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Icon(Icons.arrow_back, size: 34),
-//                     Icon(
-//                       Icons.shopping_cart,
-//                       size: 34,
-//                       color: Colors.black54,
-//                     )
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
